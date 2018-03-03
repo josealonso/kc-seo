0b1dd6484fab140ddc525dfcdd6a8cc33ecf1413
Luis Rosales <eng.luisrosales@gmail.com>
2018-03-03 23:26:17 +0100

    adding new models

--- src/cvs/admin.py
+++ src/cvs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from cvs.models import Experience, Skill, Curriculum
+
+admin.site.register(Experience)
+
+admin.site.register(Skill)
+
+admin.site.register(Curriculum)
--- src/cvs/models.py
+++ src/cvs/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+class Skill(models.Model):
+
+   name = models.CharField(max_length=50)
+   description = models.TextField(blank=True, null=True)
+
+   def __str__(self):
+       """
+       Devuelve la representación de un objeto como una string
+       """
+       return self.name
+
+class Experience(models.Model):
+
+   title = models.CharField(max_length=150)
+   summary = models.TextField()
+   company = models.CharField(max_length=250)
+   from_date = models.DateField()
+   to_date = models.DateField()
+
+   created_at = models.DateTimeField(auto_now_add=True) # set date when object is created
+   modified_at = models.DateTimeField(auto_now=True)  # saves the date when the object is updated
+
+   def __str__(self):
+       """
+       Devuelve la representación de un objeto como una string
+       """
+       return self.title
+
+class Curriculum(models.Model):
+
+   title = models.CharField(max_length=150)
+   description = models.TextField()
+   canonical = models.URLField(blank=True)
+
+   languages = CountryField(multiple=True)
+   nationality = CountryField(blank=True)
+
+   created_at = models.DateTimeField(auto_now_add=True) # set date when object is created
+   modified_at = models.DateTimeField(auto_now=True)  # saves the date when the object is updated
+
+   def __str__(self):
+       """
+       Devuelve la representación de un objeto como una string
+       """
+       return self.title
