9ab8f577b9c4f56552a1f8045fb7c12d57171202
Luis Rosales <eng.luisrosales@gmail.com>
2018-03-04 16:19:33 +0100

    creating basic webpackconfig

---
+++ src/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/js/app.js', // can be an array, a js object or single file as string. path should be relative to the config file.
+    output: {
+        path: path.resolve(__dirname, 'dist'), // we use resolve because we need an absolute path
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                // loaders are applaied to every single file
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader' // this loader is applied first
+                ]
+
+            }
+        ]
+    }
+};
